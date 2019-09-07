e58bf9de641359cbbfa4c3ad5a9d37da81cbaec3
medukr <andrii.demydyuk@gmail.com>
2019-09-07 13:28:05 +0300

    + add migration

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=stolegrator_test',
+    'username' => 'stolegrator',
+    'password' => '6betE5scVkJ96N-',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
---
+++ migrations/m190907_102417_create_user_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190907_102417_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'user_name' => $this->string()->notNull()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
