13fc8771c814e7028e17ea52a3d27ef48ee5ed4b
medukr <andrii.demydyuk@gmail.com>
2019-09-07 13:22:46 +0300

    + enable prettyUrl + add TwitterController

--- config/web.php
+++ config/web.php
@@ -43,14 +43,18 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-            ],
+                ['class' => 'yii\rest\UrlRule',
+                    'controller' => 'user',
+                    'except' => ['add', 'feed', 'remove']
+                ]
+            ]
         ],
-        */
+
     ],
     'params' => $params,
 ];
---
+++ controllers/TwitterController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by andrii
+ * Date: 07.09.19
+ * Time: 13:20
+ */
+
+namespace app\controllers;
+
+
+use yii\rest\ActiveController;
+
+class TwitterController extends ActiveController
+{
+
+    public $modelClass = '';
+}
