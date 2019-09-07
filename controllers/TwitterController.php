63db8afab6ef90a192ca0bdd425d49c516ea6c4b
medukr <andrii.demydyuk@gmail.com>
2019-09-07 13:33:15 +0300

    + add controller behaviours

--- controllers/TwitterController.php
+++ controllers/TwitterController.php
@@ -8,10 +8,40 @@
 namespace app\controllers;
 
 
+use yii\filters\VerbFilter;
 use yii\rest\ActiveController;
+use yii\web\Response;
 
 class TwitterController extends ActiveController
 {
 
     public $modelClass = '';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['contentNegotiator']['formats']['text/html'] = Response::FORMAT_JSON;
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::class,
+            'actions' => [
+                'add' => ['get'],
+                'feed' => ['get'],
+                'remove' => ['get'],
+            ]
+        ];
+        return $behaviors;
+    }
+
+
+    public function actionAdd(){
+
+    }
+
+    public function actionFeed(){
+
+    }
+
+    public function actionRemove(){
+
+    }
 }
