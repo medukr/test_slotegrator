dc9be8f9e4ee318364ccb0dec284d2e65f7094b1
medukr <andrii.demydyuk@gmail.com>
2019-09-07 13:54:08 +0300

    + add auth twitter params

--- config/params.php
+++ config/params.php
@@ -4,4 +4,10 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'auth_twitter' => [
+        'consumer_key'    => "XYNpO5yj0shMgH43j4lYKMDfH",
+        'consumer_secret' => "VevyEwrhHxabcQgN2S0KuL1i9Gx9CnPXyM2yVLfQ0LlJSZ7BmF",
+        'oauth_token'     => "3432829204-6IS6o3hGW3xouvgCso279o4ODU15grLLUy0iWPX",
+        'oauth_secret'    => "vGzYOtJkcx8PK96YcyUdXM6PtqmhGiVLmHOqCDHM2lkIq",
+    ],
 ];
