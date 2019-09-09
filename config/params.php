80c6b55520a9ea54e0a43094fcb056c60ac983ec
medukr <andrii.demydyuk@gmail.com>
2019-09-09 13:05:36 +0300

    + add SQL Dump

--- config/params.php
+++ config/params.php
@@ -5,9 +5,9 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'auth_twitter' => [
-        'consumer_key'    => "XYNpO5yj0shMgH43j4lYKMDfH",
-        'consumer_secret' => "VevyEwrhHxabcQgN2S0KuL1i9Gx9CnPXyM2yVLfQ0LlJSZ7BmF",
-        'oauth_token'     => "3432829204-6IS6o3hGW3xouvgCso279o4ODU15grLLUy0iWPX",
-        'oauth_secret'    => "vGzYOtJkcx8PK96YcyUdXM6PtqmhGiVLmHOqCDHM2lkIq",
+        'consumer_key'    => "",
+        'consumer_secret' => "",
+        'oauth_token'     => "",
+        'oauth_secret'    => "",
     ],
 ];
